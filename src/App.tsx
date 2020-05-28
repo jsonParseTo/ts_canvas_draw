3242ecea37c5fe635f0372cf3b61de8069117cfd
张文宝 <zhangwenbao@zhangwenbaodeMac-mini.local>
2020-05-28 18:25:28 +0800

    feat: 增加笔锋和曲线绘制路径优化

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,57 @@
 import React from 'react';
 import DrawSignature from './drawSignature';
-import './App.css';
+interface AppState {
+  isClearing: boolean;
+}
+class App extends React.Component <any, AppState>{
+  drawSignatureRef: any;
+  constructor(props: any) {
+    super(props);
+    this.drawSignatureRef = React.createRef();
+    this.state = {
+      isClearing: false,
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <div style={{ height: '300px', width: '100%', background: '#999' }} />
-      <DrawSignature canvasWidth="400px"  canvasHeight="200px" />
-      <div style={{ height: '1300px', width: '100%', background: '#999' }}></div>
-    </div>
-  );
+  // 切换橡皮擦的状态
+  toggleClearStatus = () => {
+    this.setState((preState) => {
+      return { isClearing: !preState.isClearing };
+    });
+  }
+
+  clearCanvas = () => {
+    this.drawSignatureRef.current.clearCanvas();
+  }
+
+  resetToPreStep = () => {
+    this.drawSignatureRef.current.resetToPreStep();
+  }
+
+  returnDrawImage = () => {
+    this.drawSignatureRef.current.returnDrawImage();
+  }
+
+  render() {
+    const { isClearing } = this.state;
+
+    return (
+      <div className="App">
+        <div style={{ height: '300px', width: '100%', background: '#999' }} />
+        <DrawSignature
+          isClearing={isClearing}
+          ref={this.drawSignatureRef}
+          canvasWidth={window.innerWidth > 600 ? '500px' : `${window.innerWidth - 100}px`}
+          canvasHeight={window.innerWidth > 600 ? '500px' : `${window.innerWidth - 100}px`}
+        />
+        <div onClick={this.clearCanvas}>清空{window.devicePixelRatio}</div>
+        <div onClick={this.resetToPreStep}>撤销</div>
+        <div onClick={this.toggleClearStatus}  style={{ color: isClearing ? '#3692f5': '#000' }}>橡皮擦</div>
+        <div onClick={this.returnDrawImage}>保存图片</div>
+        <div style={{ height: '1300px', width: '100%', background: '#999' }}></div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/drawSignature/index.tsx
+++ src/drawSignature/index.tsx
@@ -1,53 +1,95 @@
 import React from 'react';
 
-// canvas的默认宽高为300px * 150px,在css中为canvas定义宽高，实际上把宽高为300px * 150px的画布进行了拉伸。
-// 如果在这样的情况下进行canvas绘图，你得到的图形可能就是变形的效果。
+const DEVICE_PIXEL_RATIO = window.devicePixelRatio > 1 ? window.devicePixelRatio : 2; // 画布放大比例
+const MAX_SPEED = 3; // 绘制最大速率
+const MIN_SPEED = 0.1; // 绘制最小速率
 
-function midPointBtw(p1: any, p2: any) {
-  return {
-    x: p1.x + (p2.x - p1.x) / 2,
-    y: p1.y + (p2.y - p1.y) / 2
-  };
-}
-const DEVICE_PIXEL_RATIO = 2;
 interface DrawSignatureProps {
     clearWidth: number; //橡皮擦的擦拭宽度，值越大擦的越多
     canvasWidth: string;
     canvasHeight: string;
-}
-
-interface DrawSignatureState {
+    maxLineWidth: number; // 绘笔的宽度最大值
+    minLineWidth: number; // 绘笔的宽度最小值
     isClearing: Boolean; // 是否为橡皮擦状态, ture：是，false：不是（默认值）
+    drawColor: string; // 绘笔的颜色
 }
 
-class DrawSignature extends React.Component<DrawSignatureProps, DrawSignatureState> {
+interface Point {
+  x: number;
+  y: number;
+}
+
+class DrawSignature extends React.Component<DrawSignatureProps, any> {
   static defaultProps = {
-    clearWidth: 5,
+    clearWidth: 10,
+    canvasWidth: '300px',
+    canvasHeight: '150px',
+    maxLineWidth: 2,
+    minLineWidth: 1,
+    isClearing: false,
+    drawColor: '#333',
   }
 
   isDrawing = false; // 是否为绘制状态，ture：是，false：不是（默认值）
   canvasHistory : Array<String> = []; // 每次绘制完都存一份快照，撤销时需要用到
   canvasRef: any; // 存储canvas的dom节点
   ctx: any;
-  preDrawPoint: any; // 记录前一个节点的坐标
+  drawPoint: Array<Point> = []; // 记录贝塞尔曲线所需的点
+  preTimeStmp: number = 0;
+  lastLineWidth: number = -1;
+
   constructor(props: any) {
     super(props);
     this.canvasRef= React.createRef();
-    this.state = {
-      isClearing: false, 
-    };
   }
 
   componentDidMount() {
+    const { drawColor } = this.props;
+    const refRelDom = this.canvasRef.current;
+    const width = refRelDom.width;
+    const height = refRelDom.height;
     this.ctx = this.canvasRef.current.getContext('2d');
-    let width = this.canvasRef.current.width,height=this.canvasRef.current.height;
-    this.canvasRef.current.style.width = width + "px";
-    this.canvasRef.current.style.height = height + "px";
-    this.canvasRef.current.height = height * DEVICE_PIXEL_RATIO;
-    this.canvasRef.current.width = width * DEVICE_PIXEL_RATIO;
+
+    refRelDom.style.width = width + "px";
+    refRelDom.style.height = height + "px";
+    refRelDom.height = height * DEVICE_PIXEL_RATIO;
+    refRelDom.width = width * DEVICE_PIXEL_RATIO;
     this.ctx.scale(DEVICE_PIXEL_RATIO, DEVICE_PIXEL_RATIO);
-    console.log(this.canvasRef.current.height, this.canvasRef.current.width);
+    this.ctx.strokeStyle = drawColor;
+    this.ctx.lineCap = 'round';
+    this.ctx.lineJoin = 'round';
     this.ctx.fillStyle = "#ccc";
+  }
+
+  midPointBtw(p1: Point, p2: Point) {
+    return {
+      x: (p1.x + p2.x) / 2,
+      y: (p1.y + p2.y) / 2
+    };
+  }
+  
+  getPointDistance(p1: Point, p2: Point) {
+    return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+  }
+
+  calcLineWidth( time: number , distance: number ){
+    const { maxLineWidth, minLineWidth } = this.props;
+    var speed = distance / time;
+    var resultLineWidth;
+
+    if( speed <= MIN_SPEED )
+        resultLineWidth = maxLineWidth;
+    else if ( speed >= MAX_SPEED )
+        resultLineWidth = minLineWidth;
+    else{
+        resultLineWidth = maxLineWidth - (speed-MIN_SPEED)/(MAX_SPEED-MIN_SPEED)*(maxLineWidth-minLineWidth);
+    }
+
+    if( this.lastLineWidth === -1 ) {
+      return resultLineWidth;
+    }
+
+    return resultLineWidth*1/3 + this.lastLineWidth*2/3;
   }
 
   getPositionCommon = (e: any) => {
@@ -60,55 +102,64 @@ class DrawSignature extends React.Component<DrawSignatureProps, DrawSignatureSta
   }
 
   drawStart = (e: any) => {
-    console.log('start');
     e.preventDefault();
     this.isDrawing = true;
+    this.preTimeStmp = new Date().getTime();
     const { x, y } = this.getPositionCommon(e);
 
-    this.preDrawPoint = { x, y };
-    this.ctx.save();
-    this.ctx.beginPath();
-    this.ctx.moveTo(x, y);
+    this.drawPoint.push({ x, y });
   }
 
   drawMove = (e: any) =>  {
-
-    console.log('move');
     e.preventDefault();
-    const { preDrawPoint } = this;
+
+    // 如果是移动端出现多于1个手指在画布上，则取消本次任何绘制行为
+    if (e.touches && e.touches.length > 1) {
+      return;
+    }
+
     const currentPoint = this.getPositionCommon(e);
-    const { isClearing } = this.state;
-    const { clearWidth } = this.props;
+    const { clearWidth, isClearing } = this.props;
+
     if (this.isDrawing) {
-      const { x, y } = midPointBtw(currentPoint, preDrawPoint);
       if (isClearing) {
-        this.ctx.clearRect(x - clearWidth, y - clearWidth, clearWidth, clearWidth);
+        this.ctx.clearRect(currentPoint.x - clearWidth, currentPoint.y - clearWidth, clearWidth, clearWidth);
       } else {
-        console.log(x, y);
-        this.preDrawPoint = currentPoint;
-        this.ctx.strokeStyle = '#333';
-        this.ctx.lineCap = 'round';
-        this.ctx.lineJoin = 'round';
-        this.ctx.lineWidth = 2;
-        this.ctx.quadraticCurveTo(preDrawPoint.x, preDrawPoint.y, x, y);
-        this.ctx.stroke();
+        const { drawPoint } = this;
+        if (drawPoint.length <= 1) {
+          drawPoint.push(currentPoint);
+        } else {
+          const [firstPoint, secondPoint] = drawPoint;
+          const thirdPoint = this.midPointBtw(secondPoint, currentPoint);
+          const currentTimeStap = new Date().getTime();
+          const time = currentTimeStap - this.preTimeStmp;
+          const distance = this.getPointDistance(firstPoint, thirdPoint);
+          const lineWidth = this.calcLineWidth(time, distance);
+
+          this.ctx.lineWidth = lineWidth;
+          this.lastLineWidth = lineWidth;
+          this.preTimeStmp = currentTimeStap;
+          this.ctx.beginPath();
+          this.ctx.moveTo(firstPoint.x, firstPoint.y);
+          this.ctx.quadraticCurveTo(secondPoint.x, secondPoint.y, thirdPoint.x, thirdPoint.y);
+          this.drawPoint = [thirdPoint, currentPoint];
+          this.ctx.stroke();
+          this.ctx.closePath();
+        }
       }
     }
   }
 
   drawEnd = (e: any) => {
-    console.log('end');
     e.preventDefault();
 
     if (this.isDrawing) {
-
       this.ctx.closePath();
-
-      // 每次操作画布都要保存快照，后面撤销使用
+      // 每次绘制结束都要保存快照，后面撤销使用
       this.canvasHistory.push(this.saveCanvasToImage());
-      console.log(this.canvasHistory.length);
     }
     this.isDrawing = false;
+    this.drawPoint = [];
   }
 
   clearCanvas = () => {
@@ -123,12 +174,13 @@ class DrawSignature extends React.Component<DrawSignatureProps, DrawSignatureSta
 
       if (canvasHistory.length) {
         const currentCanvasIMage = canvasHistory[canvasHistory.length - 1];
+        const { width, height } = this.canvasRef.current;
         let canvasPic = new Image() as any;
 
         canvasPic.src = currentCanvasIMage;
         canvasPic.addEventListener('load', () => {
-          this.ctx.clearRect(0, 0, this.canvasRef.current.width, this.canvasRef.current.height);
-          this.ctx.drawImage(canvasPic, 0, 0, this.canvasRef.current.width / DEVICE_PIXEL_RATIO, this.canvasRef.current.height / DEVICE_PIXEL_RATIO);
+          this.ctx.clearRect(0, 0, width, height);
+          this.ctx.drawImage(canvasPic, 0, 0, width / DEVICE_PIXEL_RATIO, height / DEVICE_PIXEL_RATIO);
         });
       } else {
         this.clearCanvas();
@@ -137,28 +189,21 @@ class DrawSignature extends React.Component<DrawSignatureProps, DrawSignatureSta
   }
 
   saveCanvasToImage = () => {
-    return this.canvasRef.current.toDataURL('image/jpeg');
+    return this.canvasRef.current.toDataURL();
   }
 
   returnDrawImage = () => {
       const { canvasHistory } = this;
-      const res = canvasHistory.length ? canvasHistory[canvasHistory.length - 1] : ''; 
-      console.log(res);
-      return ;
-  }
-  // 切换橡皮擦的状态
-  toggleClearStatus = () => {
-    this.setState((preState) => {
-      return { isClearing: !preState.isClearing };
-    });
+      const res = canvasHistory.length ? canvasHistory[canvasHistory.length - 1] : '';
+
+      return res;
   }
 
   render() {
-    const { isClearing } = this.state;
     const { canvasWidth, canvasHeight } = this.props;
 
     return (
-        <div style={{ position: 'relative', overflow: 'hidden', touchAction: 'none'}}>
+        <div style={{ position: 'relative', overflow: 'hidden' }}>
           <canvas
             ref={this.canvasRef}
             // onClick={(e) => { e.preventDefault(); }}
@@ -171,12 +216,8 @@ class DrawSignature extends React.Component<DrawSignatureProps, DrawSignatureSta
             onMouseOut={this.drawEnd}
             width={canvasWidth}
             height={canvasHeight}
-            style={{ userSelect: 'none' }}
+            style={{ userSelect: 'none', backgroundColor: '#eee', touchAction: 'none' }}
           />
-          <div onClick={this.clearCanvas}>清空</div>
-          <div onClick={this.resetToPreStep}>撤销</div>
-          <div onClick={this.toggleClearStatus}  style={{ color: isClearing ? '#3692f5': '#000' }}>橡皮擦</div>
-          <div onClick={this.returnDrawImage}>保存图片</div>
         </div>
     )
   }
